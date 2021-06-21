c858a3f40b738ffb7a58b5fa4a59d1cc9b0b9128
Christoph Z <cziegler@hm.edu>
2021-06-21 15:10:42 +0200

    added profileDefinitionBackendService

---
+++ src/SEIIApp/Client/Service/ProfileDefinitionBackendAccessService.cs
@@ -0,0 +1,42 @@
+using SEIIApp.Shared.DomainTdo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SEIIApp.Client.Service
+{
+    public class LessonDefinitionBackendAccessService
+    {
+        private HttpClient HttpClient { get; set; }
+
+        public LessonDefinitionBackendAccessService(HttpClient client)
+        {
+            this.HttpClient = client;
+        }
+
+        private string GetLessonDefinitionUrl()
+        {
+            return "api/lessondefinitions";
+        }
+        private string GetLessonDefinitionWithId(int id)
+        {
+            return $"{GetLessonDefinitionUrl()}/{id}";
+        }
+        public async Task<LessonDefinitionDto[]> GetLessonOverview()
+        {
+            return await HttpClient.GetFromJsonAsync<LessonDefinitionDto[]>(GetLessonDefinitionUrl());
+        }
+
+        public async Task<LessonDefinitionDto> GetLessonById(int id)
+        {
+            return await HttpClient.GetFromJsonAsync<LessonDefinitionDto>(GetLessonDefinitionWithId(id));
+        }
+
+
+    }
+}
